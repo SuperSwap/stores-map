ae3d8eef374b33166739f5abb1787857614951e6
SuperSwap <dukreswapnil@gmail.com>
2023-02-16 23:18:20 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,119 @@
+var data_features = {
+    "type": "FeatureCollection",
+    "crs": {
+        "type": "name",
+        "properties": {
+            "name": "urn:ogc:def:crs:OGC:1.3:CRS84"
+        }
+    },
+    "features": [
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "ak16994571",
+                "name": "abc",
+                "address": "pqr",
+                "phone": "6779",
+                "email": "a@abc.com"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -151.5129,
+                    65.1016,
+                    0.0
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "ak16994569",
+                "name": "xyz",
+                "address": "lmn",
+                "phone": "1234",
+                "email": "a@xyz.com"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -150.4048,
+                    73.1224,
+                    105.5
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "ak16994559",
+                "name": "xyz",
+                "address": "lmn",
+                "phone": "1234",
+                "email": "a@xyz.com"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -160.4048,
+                    63.1224,
+                    105.5
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "ak16994549",
+                "name": "xyz",
+                "address": "lmn",
+                "phone": "1234",
+                "email": "a@xyz.com"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -120.4048,
+                    63.1224,
+                    105.5
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "ak16994539",
+                "name": "xyz",
+                "address": "lmn",
+                "phone": "1234",
+                "email": "a@xyz.com"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -110.4048,
+                    43.1224,
+                    105.5
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "ak16994529",
+                "name": "xyz",
+                "address": "lmn",
+                "phone": "1234",
+                "email": "a@xyz.com"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -110.4048,
+                    33.1224,
+                    105.5
+                ]
+            }
+        }
+    ]
+}
